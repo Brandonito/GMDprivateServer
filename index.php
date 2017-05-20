68eb68c1bf4aee27cda8cad26d5176401fcabd7c
Brandonito <brandon_n4@hotmail.com>
2017-05-20 11:49:23 -0500

    Nuevo!!  Index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+</html>
+<title>Welcome</title>
+<body background="Images/gradientBG.png" bgcolor="#999999">
+<dt><label title="Bienvenido al Servidor">Bienvenido al servidor </label></dt>
+
+<ul>
+	<li><a href="tools/account/changePassword.php" title="Cambiar Pass">Cambiar Pass?</a></li>
+	<li><a href="tools/account/changeUsername.php" title="Cambiar Username">Cambiar Username?</a></li>
+
+  </ul>
